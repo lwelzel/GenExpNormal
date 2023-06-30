ce6e9e26d7fb20318782138de356138f4807c68c
lwelzel <29613344+lwelzel@users.noreply.github.com>
2023-06-30 14:44:18 +0200

    PDF approximation to make over/underflow robust
    Might break batching if some distributions have a rate of 0

--- expnormal.py
+++ expnormal.py
@@ -12,7 +12,7 @@ class ExponentiallyModifiedNormal(Distribution):
     Args:
         loc (Tensor): Mean of the normal distribution.
         scale (Tensor): Standard deviation of the normal distribution.
-        rate (Tensor): Rate parameter of the exponential distribution used for modification.
+        tau (Tensor): Relaxation time parameter of the exponential distribution used for modification.
         validate_args (bool, optional): Whether to enable parameter validation. Default: None.
 
     Note:
@@ -21,34 +21,47 @@ class ExponentiallyModifiedNormal(Distribution):
     """
     arg_constraints = {'loc': constraints.real,
                        'scale': constraints.positive,
-                       'rate': constraints.real}
+                       'tau': constraints.real}
     support = constraints.real
     has_rsample = True
 
     def __init__(self,
                  loc: Tensor = torch.tensor([0.]),
                  scale: Tensor = torch.tensor([1.]),
-                 rate: Tensor = torch.tensor([0.]),
+                 tau: Tensor = torch.tensor([0.]),
                  validate_args: bool = None) -> None:
 
-        self.loc, self.scale, self.rate = broadcast_all(loc, scale, rate)
+        self.loc, self.scale, self.tau = broadcast_all(loc, scale, tau)
+        self.rate = torch.sign(tau) * 1 / (torch.abs(tau) + 1.e-8)
         super().__init__(validate_args=validate_args)
 
         # get sign of rate to change the modification
-        self.sign = torch.sign(self.rate)
-
+        self.sign = torch.sign(self.tau)
+        self._tau = torch.abs(self.tau)
         self._rate = torch.abs(self.rate)
 
         # set all distributions where there should be no exponential modification to a valid rate
         try:
             self._rate[torch.argwhere(self._rate == 0.)] = 1.
         except IndexError:
-            if self._rate == 0.:
-                self._rate = 1.
+            if self._rate.item == 0.:
+                self._rate = torch.tensor([1.])
 
         self.normal = Normal(loc=self.loc, scale=self.scale)
+        self.gaussian_amplitude = torch.exp(self.normal.log_prob(self.loc))
 
         self.exponential = Exponential(rate=self._rate)
+
+    def __repr__(self):
+        s = f"Exponentially Modified Normal distribution:\n" \
+            f"\t LOC:    {self.loc.item():.3e}                \n" \
+            f"\t SCALE:  {self.scale.item():.3e}              \n" \
+            f"\t TAU:    {self.tau.item():.3e}                \n" \
+            f"\t RATE:   {self.rate.item():.3e}               \n" \
+            f"\t SIGN:   {self.sign.item():.3e}               \n" \
+            f"\t |RATE|: {self._rate.item():.3e}              \n" \
+            f"\t N-AMPL: {self.gaussian_amplitude.item():.3e} \n"
+        return s
 
     def sample(self, sample_shape: torch.Size() = torch.Size()) -> Tensor:
         with torch.no_grad():
@@ -58,9 +71,45 @@ class ExponentiallyModifiedNormal(Distribution):
         return self.sign * (self.normal.rsample(sample_shape) + self.exponential.rsample(sample_shape))
 
     def pdf(self, value: Tensor) -> Tensor:
+        value = self.sign * value
+
+        output = torch.zeros_like(value)
+
+        shift = (value - self.loc) / self.scale
+        z = 1 / 1.41421356237 * (self.scale / self._tau - shift)
+        # case 0: |z| = 0
+        if self.rate == 0.:
+            assert len(value.shape) == 1., "Only non batched input for this for now."
+            return torch.exp(self.normal.log_prob(self.sign * value))
+
+        # case 1: z < 0
+        mask1 = torch.logical_and(z < 0., z >= -6.71e7)
+        if torch.sum(mask1) > 0.:
+            output[mask1] = self.gaussian_amplitude * self.scale / self._tau * torch.sqrt(torch.pi / torch.tensor([2.])) \
+                           * torch.exp(torch.tensor([0.5]) * torch.pow(self.scale / self._tau, 2.)
+                                       - (value[mask1] - self.loc) / self._tau) \
+                           * torch.erfc(z[mask1])
+
+        # case 2: 0 <= z <= 6.71 x 10^7
+        mask2 = torch.logical_and(z >= 0., z <= 6.71e7)
+        if torch.sum(mask2) > 0.:
+            output[mask2] = self.gaussian_amplitude * self.scale / self._tau * torch.sqrt(torch.pi / torch.tensor([2.])) \
+                            * torch.exp(- torch.tensor([0.5]) * torch.pow(shift[mask2], 2.)) \
+                            * torch.special.erfcx(z[mask2])
+
+        # case 3: z > 6.71 x 10^7
+        mask3 = torch.logical_or(z >= 6.71e7, z <= -6.71e7)
+        if torch.sum(mask3) > 0.:
+            output[mask3] = self.gaussian_amplitude * torch.exp(- torch.tensor([0.5]) * torch.pow(shift[mask3], 2.)) \
+                            / (torch.tensor([1.]) + self._tau * shift[mask3] / self.scale, 2)
+
+        return output
+
+    def _pdf(self, value: Tensor) -> Tensor:  # depreciated
+        value = self.sign * value
         return self._rate / 2 \
-            * torch.exp(self._rate / 2 * (2 * self.loc + self._rate * self.scale ** 2. - 2. * self.sign * value)) \
-            * torch.erfc((self.loc + self._rate * self.scale ** 2. - self.sign * value) / (1.41421356237 * self.scale))
+            * torch.exp(self._rate / 2 * (2 * self.loc + self._rate * self.scale ** 2. - 2. * value)) \
+            * torch.erfc((self.loc + self._rate * self.scale ** 2. - value) / (1.41421356237 * self.scale))
 
     def log_prob(self, value: Tensor) -> Tensor:
         return torch.log(self.pdf(value))
@@ -92,21 +141,23 @@ if __name__ == '__main__':
     # batch sampling is implemented for mixed positive and negative rate distributions and fully normal ones (rate = 0)
     loc = torch.tensor([0.])
     scale = torch.tensor([1.])
-    rate = torch.tensor([-1.])
+    tau = torch.tensor([-1.])
 
-    generalized_normal = ExponentiallyModifiedNormal(loc=loc, scale=scale, rate=rate)
+    generalized_exponential_normal = ExponentiallyModifiedNormal(loc=loc, scale=scale, tau=tau)
+    print(generalized_exponential_normal)
 
-    samples = generalized_normal.sample((10000, ))
+    samples = generalized_exponential_normal.sample((10000, ))
 
-    x_space = torch.linspace(-10, 5, 100)
-    pdf = generalized_normal.pdf(x_space)
+    x_space = torch.linspace(samples.min(), samples.max(), 1000)
+    pdf = generalized_exponential_normal.pdf(x_space)
+    _pdf = generalized_exponential_normal._pdf(x_space)
 
     fig, ax = plt.subplots(1, 1, constrained_layout=True, figsize=(8, 4))
 
     ax.hist(samples.cpu().numpy().flatten(), bins=50, alpha=0.8, color="red", density=True, label="Samples", zorder=0)
 
-    ax.scatter(x_space[30:80:2], pdf[30:80:2], label="PDF", c="black", s=4, marker="D")
-    ax.plot(x_space, pdf, ls="dashed", c="black", lw=0.75)
+    ax.scatter(x_space[::20], pdf[::20], label="PDF", c="black", s=4, marker="D")
+    ax.plot(x_space, pdf, ls="dashed", c="black", lw=0.75, label="PDF")
 
     ax.set_ylabel("Density")
     ax.set_xlabel(r"$Z_{Gen.~EMG} = X_{\mathcal{N}} + Y_{Exp}$")
